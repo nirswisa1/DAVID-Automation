d2dc67d9d1c7be54ce689b0da273285c3ca81497
nirswisa1 <91661470+nirswisa1@users.noreply.github.com>
2021-10-26 14:33:28 +0300

    Update david excel.py

--- david excel.py
+++ david excel.py
@@ -1,16 +1,43 @@
 from openpyxl import load_workbook
 import pandas as pd
 import matplotlib.pyplot as plt
+from tkinter import *
+#GUI
+root = Tk()
+root.title('Nir DAVID excel')
+fname=''
 
-file_name='enter file name'
-wb = load_workbook(file_name)
+def myClick():
+    global fname
+    fname=fnameE.get()
+    root.destroy()
+    return fname
+    
+#slots
+fnameE = Entry(root,width=50,borderwidth=2)
+fnameE.grid(row=0,column = 1)
+
+#labels
+label1 = Label(root,text='File name: ')
+
+label1.grid(row=0,column = 0)
+
+#Button
+b = Button(root,text = "Run",command = myClick)
+b.grid(row=1,column = 1)
+
+root.mainloop()
+
+#excel
+print(fname+'.xlsx')
+wb = load_workbook(fname+'.xlsx')
 ws = wb.active
 added = []
 
 
 def funcx(current):
     global added
-    global file_name
+    global fname
     gene_names = ws["F" + str(current)].value
     for row in range(current, 50):
         cur_check = ws["F" + str(row)].value
@@ -22,12 +49,11 @@ def funcx(current):
             ws["O" + str(current)].value = (str(ws["O" + str(current)].value) + ", " + cur_gene_name)[5:]
             a = ws["O" + str(current)].value
             added.append(cur_gene_name)
-    wb.save(file_name)
+    wb.save(fname)
 
 
 current = 1
 for cell in ws['F']:
-    print(current)
     if cell.value is None or cell.value == "Genes":
         added = []
         current += 1
