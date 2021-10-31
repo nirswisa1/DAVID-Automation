715b08543ca2e9a90637966dd6fca654de6f7ae0
nirswisa1 <91661470+nirswisa1@users.noreply.github.com>
2021-10-31 19:20:52 +0200

    Update david excel.py

--- david excel.py
+++ david excel.py
@@ -2,35 +2,36 @@ from openpyxl import load_workbook
 import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import *
-#GUI
+
+# GUI
 root = Tk()
-root.title('Nir DAVID excel')
-fname=''
+root.title('Nir.S - DAVID Excel')
+fname = ''
+
 
 def myClick():
     global fname
-    fname=fnameE.get()
+    fname = fnameE.get()
     root.destroy()
     return fname
-    
-#slots
-fnameE = Entry(root,width=50,borderwidth=2)
-fnameE.grid(row=0,column = 1)
 
-#labels
-label1 = Label(root,text='File name: ')
 
-label1.grid(row=0,column = 0)
+# slots
+fnameE = Entry(root, width=50, borderwidth=2)
+fnameE.grid(row=0, column=1)
 
-#Button
-b = Button(root,text = "Run",command = myClick)
-b.grid(row=1,column = 1)
+# labels
+label1 = Label(root, text='File name: ')
+label1.grid(row=0, column=0)
+
+# Button
+b = Button(root, text="Run", command=myClick)
+b.grid(row=1, column=1)
 
 root.mainloop()
 
-#excel
-print(fname+'.xlsx')
-wb = load_workbook(fname+'.xlsx')
+# excel
+wb = load_workbook(fname + '.xlsx')
 ws = wb.active
 added = []
 
@@ -38,29 +39,49 @@ added = []
 def funcx(current):
     global added
     global fname
-    gene_names = ws["F" + str(current)].value
+    gene_names = ws["F" + str(current)].value  # ['QIHEL'...]
     for row in range(current, 50):
-        cur_check = ws["F" + str(row)].value
-        cur_gene_name = ws["B" + str(row)].value
+        cur_check = ws["F" + str(row)].value  # ['QIFFF'...]
+        cur_gene_name = ws["B" + str(row)].value  # ['GO:302198...']
         if cur_check is None:
             break
         if cur_check == gene_names and cur_gene_name not in added:
             added.append(cur_gene_name)
-            ws["O" + str(current)].value = (str(ws["O" + str(current)].value) + ", " + cur_gene_name)[5:]
-            a = ws["O" + str(current)].value
-            added.append(cur_gene_name)
-    wb.save(fname)
+            if ws["O" + str(current)].value is None:
+                if cur_gene_name[0] == "G":
+                    ws["O" + str(current)].value = (str(ws["O" + str(current)].value) + ", " + cur_gene_name[
+                                                                                               cur_gene_name.find(
+                                                                                                   '~') + 1:])[5:]
+                elif cur_gene_name[0] == "m":
+                    ws["O" + str(current)].value = (str(ws["O" + str(current)].value) + ", " + cur_gene_name[
+                                                                                               cur_gene_name.find(
+                                                                                                   ':') + 1:])[5:]
+            elif cur_gene_name[0] == "G":
+                ws["O" + str(current)].value = str(ws["O" + str(current)].value) + ", " + cur_gene_name[
+                                                                                          cur_gene_name.find('~') + 1:]
+            elif cur_gene_name[0] == "m":
+                ws["O" + str(current)].value = str(ws["O" + str(current)].value) + ", " + cur_gene_name[
+                                                                                          cur_gene_name.find(':') + 1:]
 
+    wb.save(fname + '.xlsx')
 
+#Main loop
+Lcolors = []
 current = 1
+colorNum = -1
 for cell in ws['F']:
-    if cell.value is None or cell.value == "Genes":
+    if cell.value is None:
         added = []
         current += 1
+    elif cell.value == "Genes":
+        added = []
+        current += 1
+        colorNum += 1
     else:
+        Lcolors.append(colorNum)
         funcx(current)
         current += 1
-
+#Plot
 names = []
 numbers = []
 for num, cell in enumerate(ws["O"]):
@@ -68,8 +89,9 @@ for num, cell in enumerate(ws["O"]):
         names.append(str(cell.value))
         numbers.append(ws["C" + str(num + 1)].value)
 
-new_data = {'names': names, 'numbers': numbers}
+colors = ['teal', 'powderblue', 'lightgreen', 'cadetblue', 'paleturquoise', 'palegreen']
+new_data = {'names': list(reversed(names)), 'numbers': list(reversed(numbers))}
 df = pd.DataFrame.from_dict(new_data);
 df
-plt.barh(df.names, df.numbers)
+plt.barh(df.names, df.numbers, color=[colors[i] for i in Lcolors])
 plt.show()
